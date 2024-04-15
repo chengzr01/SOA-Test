2e4f2a7b3121d30c5e053431deb2bcfc5f76ee08
chengzr01 <ziruicheng01@gmail.com>
2024-04-16 00:32:34 +0800

    modify response

--- soa/agent/views.py
+++ soa/agent/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the agent index.")
+    return JsonResponse({'code': 200, 'data': "Hello"}, status=200)
